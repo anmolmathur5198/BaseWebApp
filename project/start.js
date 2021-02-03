8e323937ab19495cb25def555467e6564721464e
Anmol Mathur <mathur.anmol@dotsquares.com>
2021-02-03 16:35:37 +0530

    changes

--- project/start.js
+++ project/start.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-
+var axios = require('axios')
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
@@ -13,6 +15,24 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post("/secure", function (request, response) {
+ 
+  console.log('checkingDtaa',request.body)
+  response.send({success:true,data:'Working'});
+});
+
+app.get('/webhook', function(req,res){
+  var data = {
+    result:[{firstname:'Anmol',lastname:'maaa'}]
+  }
+  axios.post('',data).then((result)=>{
+    console.log(result)
+    return res.send({success:true,data:'Completed'})
+  }).catch((err)=>{
+    console.log(err)
+    return res.send({success:false,err})
+  })
+})
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
